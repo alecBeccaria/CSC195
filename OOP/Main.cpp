5eebf551c7162004ec80d9fee12b72e7d669abd7
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-04-19 10:52:41 -0600

    Update Main.cpp

--- OOP/Main.cpp
+++ OOP/Main.cpp
@@ -1,14 +1,28 @@
 #include "Bird.h"
 #include "Reptile.h"
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+	vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_back(4);
 	//Animal animal;
 	//animal.Speak();
+	vector<Animal*> animals;
+	animals.push_back(new Bird(12, 14.0f));
+	animals.push_back(new Bird(10, 20.0f));
+	animals.push_back(new Reptile(5));
+
+	for (Animal* animal : animals)
+	{
+		animal->Speak();
+	}
 	
 	Bird bird(12, 10.0f);
 	bird.Speak();
