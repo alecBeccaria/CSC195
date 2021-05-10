2ea6fa56b7aee7307f9f6aeb4a5ec5f115e958d3
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-05-10 15:32:17 -0600

    Vector Assignment

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -14,7 +14,7 @@ namespace nc
         vector(const vector& other);
         ~vector();
 
-        //vector& operator = (const vector& other);
+        vector& operator = (const vector& other);
 
         T& operator [] (size_t position) { return _elements[position];  }
         const T& operator [] (size_t position) const { return _elements[position]; }
@@ -31,10 +31,10 @@ namespace nc
         void pop_back();
 
         void reserve(size_t capacity);
-        void resize(size_t new_size, const T& value);
+        void resize(size_t new_size, const T& value = T{});
         void clear();
 
-        void swap(vector& other) {}
+        void swap(vector& other);
 
         bool empty() const { return (_size == 0); }
         size_t size() const { return _size; }
@@ -51,16 +51,33 @@ namespace nc
     vector<T>::vector(size_t capacity)
     {
         _elements = new T[capacity];
+        _capacity = capacity;
     }
     template<typename T>
     vector<T>::vector(const std::initializer_list<T>& ilist)
     {
+        _size = ilist.size();
+        _capacity = ilist.size();
+        _elements = new T[_capacity];
 
+        size_t i = 0;
+        for (auto iter = ilist.begin(); iter != ilist.end(); iter++)
+        {
+            _elements[i] = *iter;
+            i++;
+        }
     }
     template<typename T>
     vector<T>::vector(const vector& other)
     {
+        _size = other._size;
+        _capacity = other.capacity;
+        _elements = new T[_capacity];
 
+        for (size_t i = 0; i < _size; i++)
+        {
+            _elements[i] = other._elements[i];
+        }
     }
     template<typename T>
     vector<T>::~vector()
@@ -70,6 +87,25 @@ namespace nc
             delete[] _elements;
         }
         
+    }
+
+    template<typename T>
+    vector<T>& vector<T>::operator = (const vector& other)
+    {
+        if (this == &other) return *this;
+
+        if(_elements) delete[] _elements;
+
+        _size = other._size;
+        _capacity = other._capacity;
+        _elements = new T[_capacity];
+
+        for (size_t i = 0; i < _size; i++)
+        {
+            _elements[i] = other._elements[i];
+        }
+
+        return *this;
     }
 
     template<typename T>
@@ -107,15 +143,53 @@ namespace nc
         _elements = new_elements;
         _capacity = capacity;
     }
+
     template<typename T>
     void vector<T>::resize(size_t new_size, const T& value)
     {
+        if (new_size > _size)
+        {
+            if (new_size > _capacity)
+            {
+                reserve(new_size);
+            }
 
+            for (size_t i = _size; i < new_size; i++)
+            {
+                _elements[i] = value;
+            }
+        }
+        
+        _size = new_size;
     }
+
     template<typename T>
     void vector<T>::clear()
     {
+        if (_elements)
+        {
+            delete[] _elements;
+        }
+        
+        _elements = nullptr;
+        _capacity = 0;
+        _size = 0;
+    }
 
+    template<typename T>
+    void vector<T>::swap(vector& other)
+    {
+        T* temp_elements = _elements;
+        size_t temp_size = _size;
+        size_t temp_capacity = _capacity;
+
+        _elements = other._elements;
+        _size = other._size;
+        _capacity = other._capacity;
+
+        other._elements = temp_elements;
+        other._size = temp_size;
+        other._capacity = temp_capacity;
     }
 
 }
